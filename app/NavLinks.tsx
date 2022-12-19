f35c0fde9c1c811e99383bf006695d3b1a6a0cd7
imtiyaz-linux <mohammedimtiyaz.jk@yahoo.com>
2022-12-19 09:17:34 +0530

    initial setup, header

---
+++ app/NavLinks.tsx
@@ -0,0 +1,30 @@
+"use client";
+import React from "react";
+import NavLink from "./NavLink";
+import { usePathname } from "next/navigation";
+
+const categories: string[] = [
+  "general",
+  "business",
+  "entertainment",
+  "health",
+  "science",
+  "sports",
+  "technology",
+];
+
+function NavLinks() {
+  const pathName = usePathname();
+  const isActive = (path: string) => {
+    return pathName?.split("/").pop() === path;
+  };
+  return (
+    <nav className="grid grid-cols-4 md:grid-cols-7 text-xs md:text-sm gap-4 pb-10 max-w-6xl mx-auto border-b">
+      {categories.map((cat) => (
+        <NavLink key={cat} isActive={isActive(cat)} category={cat} />
+      ))}
+    </nav>
+  );
+}
+
+export default NavLinks;
